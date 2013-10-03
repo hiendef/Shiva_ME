689db69dfa6c84130d31b9db42bc66a0f960d5d1
hiendef <hiendef@gmail.com>
2013-10-03 12:47:10 +0700

    Commit 1 - Shiva ME

---
+++ src/shiva/gauxam/com/ShivaMidlet.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shiva.gauxam.com;
+
+import com.sun.lwuit.Display;
+import javax.microedition.midlet.*;
+
+/**
+ * @author hien
+ */
+public class ShivaMidlet extends MIDlet {
+
+    public void startApp() {
+        Display.init(this);
+        
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
