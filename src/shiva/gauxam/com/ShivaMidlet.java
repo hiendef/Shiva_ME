f487d66c3249ad4f1a8a0ddd946db3cc16d0a509
hiendef <hiendef@gmail.com>
2013-10-08 02:09:10 +0700

    hiện thị GUI thành công ^^

--- src/shiva/gauxam/com/ShivaMidlet.java
+++ src/shiva/gauxam/com/ShivaMidlet.java
@@ -6,6 +6,11 @@ package shiva.gauxam.com;
 
 import com.sun.lwuit.Display;
 import com.sun.lwuit.Form;
+import com.sun.lwuit.plaf.UIManager;
+import com.sun.lwuit.util.Resources;
+import com.sun.lwuit.util.UIBuilder;
+import java.io.IOException;
+import java.util.Hashtable;
 import javax.microedition.midlet.*;
 
 /**
@@ -15,7 +20,10 @@ public class ShivaMidlet extends MIDlet {
 
     public void startApp() {
         Display.init(this);
-        
+        UIBuilder b=new UIBuilder();
+        b.setResourceFilePath("/gui/GUI_240x320.res");
+        Form connect=b.showForm("Connect_GUI", null);
+        connect.show();     
     }
     
     public void pauseApp() {
