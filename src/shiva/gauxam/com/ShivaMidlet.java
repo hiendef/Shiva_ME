de6f5b02ab6a4314f3bb28203ce6978d1e4c19db
hiendef <hiendef@gmail.com>
2013-10-08 03:02:29 +0700

    ket noi thanh cong voi server ^^

--- src/shiva/gauxam/com/ShivaMidlet.java
+++ src/shiva/gauxam/com/ShivaMidlet.java
@@ -9,15 +9,23 @@ import com.sun.lwuit.Form;
 import com.sun.lwuit.plaf.UIManager;
 import com.sun.lwuit.util.Resources;
 import com.sun.lwuit.util.UIBuilder;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.util.Hashtable;
+import javax.microedition.io.Connector;
 import javax.microedition.midlet.*;
 
 /**
  * @author hien
  */
 public class ShivaMidlet extends MIDlet {
-
+    private String serverName;
+    private String serverPort;
+    public ShivaMidlet()
+    {
+        this.serverName="http://127.0.0.1";
+        this.serverPort="9009";
+    }
     public void startApp() {
         Display.init(this);
         Resources res = null;
@@ -29,8 +37,19 @@ public class ShivaMidlet extends MIDlet {
         UIManager.getInstance().setThemeProps(res.getTheme("Connect_THEME"));
         UIBuilder b=new UIBuilder();
         b.setResourceFilePath("/gui/GUI_240x320.res");
-        Form connect=b.showForm("Connect_GUI", null);
-        connect.show();     
+        Form f=b.showForm("Connect_GUI", null);
+        f.show();
+        try {
+            // connect to server_name: localhost_port: 9009
+            DataInputStream dataIn= Connector.openDataInputStream(serverName+":"+serverPort);
+            if(dataIn!=null)
+            {
+                
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
     }
     
     public void pauseApp() {
